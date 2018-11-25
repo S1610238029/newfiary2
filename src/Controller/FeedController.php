7b0146d2a8643661286a41b6b31c16d5ff0a5c48
S1610238029 <birgit_haselmayr@aon.at>
2018-11-25 21:57:27 +0100

    feed edit and delete

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -63,5 +63,53 @@ class FeedController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+
+    /**
+     * @Route("/delete/{id}", name="feed_delete")
+     */
+    public function deleteFeed($id)
+    {
+        $feed=$this->getDoctrine()->getRepository(Feed::class)->find($id);
+
+        if($feed){
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($feed);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('blackboard');
+
+    }
+
+    /**
+     * @Route("/edit/{id}", name="feed_edit")
+     */
+    public function editFeed($id, Request $request){
+        $feed=$this->getDoctrine()->getRepository(Feed::class)->find($id);
+
+        if($feed){
+            $form = $this->buildForm($feed);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+
+                $feed = $form->getData();
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($feed);
+                $em->flush();
+
+                return $this->redirectToRoute('blackboard');
+
+            }
+            return $this->render('pages/blackboard_neu.html.twig', [
+                'form' => $form->createView(),
+            ]);
+
+        }
+
+        return $this->redirectToRoute('pizza_index');
+    }
 }
 
