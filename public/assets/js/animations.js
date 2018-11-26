15793fe29dafce1072a8da856b6927e0a1aba310
Isabella <s1610238056@students.fh-hagenberg.at>
2018-11-26 14:39:10 +0100

    fixed sidebar

--- public/assets/js/animations.js
+++ public/assets/js/animations.js
@@ -1,11 +1,12 @@
 function closeNav(){
 
     var sidebar = document.getElementById("sidebar").classList.toggle("active");
+    document.getElementById("placeholder").classList.toggle("active");
     document.getElementById("arrow").classList.toggle("active");
     console.log("toggle");
 
     if (sidebar.hasClass("active")){
-        
+
     }
 
 
