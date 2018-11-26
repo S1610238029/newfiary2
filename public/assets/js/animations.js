7d82c4bbc2ddc1fcab6b6dd3188bfa8887501e8a
Isabella <s1610238056@students.fh-hagenberg.at>
2018-11-26 15:32:43 +0100

    animation navbar

--- public/assets/js/animations.js
+++ public/assets/js/animations.js
@@ -1,18 +1,7 @@
 function closeNav(){
-
     var sidebar = document.getElementById("sidebar").classList.toggle("active");
     document.getElementById("placeholder").classList.toggle("active");
     document.getElementById("arrow").classList.toggle("active");
     console.log("toggle");
-
-    if (sidebar.hasClass("active")){
-
-    }
-
-
-    let element = document.getElementById("sidebar");
-    element.addEventListener("transitionend", function(event) {
-
-    }, false);
 }
 
