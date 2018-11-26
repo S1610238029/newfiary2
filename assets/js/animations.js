cf43ecea53d6ce416d7939dc6e5b519000770baa
Isabella <s1610238056@students.fh-hagenberg.at>
2018-11-26 13:43:59 +0100

    Revert "Revert "burger menu responsive""
    This reverts commit 7e7ba9099b86e51f748745a1bc62612c93d49de1.

--- assets/js/animations.js
+++
@@ -1,7 +0,0 @@
-function closeNav(){
-
-    document.getElementById("sidebar").classList.toggle("active");
-    document.getElementById("arrow").classList.toggle("active");
-    console.log("toggle");
-
-}
