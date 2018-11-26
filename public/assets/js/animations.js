45ec3465913859d231a949fe1fd4b30dc2d5c9a2
S1610238029 <birgit_haselmayr@aon.at>
2018-11-26 18:15:32 +0100

    mitgliederliste

--- public/assets/js/animations.js
+++ public/assets/js/animations.js
@@ -5,3 +5,20 @@ function closeNav(){
     console.log("toggle");
 }
 
+$(document).ready(function() {
+    $('#example').DataTable( {
+        "order": [[ 3, "asc" ]],
+        "stateSave": true,
+        "language": {
+            "lengthMenu": "Zeige _MENU_ Einträge pro Seite",
+            "zeroRecords": "Nichts gefunden - sorry",
+            "info": "Seite _PAGE_ von _PAGES_",
+            "infoEmpty": "Keien Einträge verfügbar",
+            "infoFiltered": "(gefiltert von _MAX_ Gesamteinträgen)",
+            "search": "Suchen",
+            "paginatePrevious": "Vorherige"
+        }
+
+    } );
+} );
+
